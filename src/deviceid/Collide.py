92cb92af2169a0f18bfabf100e0d6b06b7173f49
chensongsong <chensongsong@geetest.com>
2020-03-05 12:28:00 +0800

    init

---
+++ src/deviceid/Collide.py
@@ -0,0 +1,18 @@
+import unittest
+
+'''
+指纹碰撞分析
+'''
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+def parseJson():
+    return;
+
+
+if __name__ == '__main__':
+    unittest.main()
